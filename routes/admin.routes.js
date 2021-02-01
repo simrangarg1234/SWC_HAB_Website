a9560e597c43848cbc8846c2d0945496a8f2b18b
simrangarg1234 <lakshaygarg1357@gamil.com>
2021-02-01 20:39:20 +0530

    form added

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const middleware = require("../middleware");
 const User = require("../models/user");
 const Notice = require("../models/notice");
+const Form = require("../models/forms");
 const Announcement = require("../models/announcement");
 const multer = require("multer");
 const storage = multer.diskStorage({
@@ -31,6 +32,12 @@ router.get("/notice", middleware.isLoggedIn, async (req, res) => {
   const notices = await Notice.find({});
   notices.sort(compare);
   res.render("notice", { notices });
+});
+
+router.get("/form", middleware.isLoggedIn, async (req, res) => {
+  const forms = await Form.find({});
+  forms.sort(compare);
+  res.render("form", { forms });
 });
 
 router.get("/announcement", middleware.isLoggedIn, async (req, res) => {
@@ -80,11 +87,20 @@ router.post(
   middleware.isLoggedIn,
   upload.single("notice"),
   async (req, res) => {
-    const { title, description } = req.body;
+    const { title, description, imp } = req.body;
     const path = req.file.filename;
+    var important=0;
+    if(imp != undefined) {
+      important=1;
+    }
 
     const newNotice = new Notice({ title, description, path });
     await newNotice.save();
+
+    if(important) {
+        const newAnnouncement = new Announcement({ description, path, important });
+        await newAnnouncement.save();
+    }
 
     res.redirect("/notice");
   }
@@ -116,6 +132,17 @@ router.delete("/notice/:id", middleware.isLoggedIn, async (req, res) => {
   }
 });
 
+// router.patch("/notice/:id/",middleware.isLoggedIn, async (req,res) => {
+//   Notice.update({id: req.params.id}, {$set: req.body}, function(err) {
+//     if(!err) {
+//       res.redirect("/notices");
+//     }
+//     else {
+//       console.log(err);
+//     }
+//   });
+// });
+
 router.get("/announcement/add", middleware.isLoggedIn, (req, res) => {
   res.render("announcement_add");
 });
@@ -125,13 +152,17 @@ router.post(
   middleware.isLoggedIn,
   upload.single("announcement"),
   async (req, res) => {
-    const { description } = req.body;
+    const { description,imp } = req.body;
+    var important=0;
+    if(imp != undefined) {
+      important=1;
+    }
     if (typeof req.file !== "undefined") {
       const path = req.file.filename;
-      const newAnnouncement = new Announcement({ description, path });
+      const newAnnouncement = new Announcement({ description, path, important });
       await newAnnouncement.save();
     } else {
-      const newAnnouncement = new Announcement({ description });
+      const newAnnouncement = new Announcement({ description, important });
       await newAnnouncement.save();
     }
 
@@ -169,6 +200,61 @@ router.delete("/announcement/:id", middleware.isLoggedIn, async (req, res) => {
     res.redirect("/announcement");
   }
 });
+
+router.get("/form/add", middleware.isLoggedIn, (req, res) => {
+  res.render("form_add");
+});
+
+router.post(
+  "/form",
+  middleware.isLoggedIn,
+  upload.single("form"),
+  async (req, res) => {
+    const { title, description, imp } = req.body;
+    const path = req.file.filename;
+    var important=0;
+    if(imp != undefined) {
+      important=1;
+    }
+
+    const newForm = new Form({ title, description, path });
+    await newForm.save();
+
+    if(important) {
+        const newAnnouncement = new Announcement({ description, path, important });
+        await newAnnouncement.save();
+    }
+
+    res.redirect("/form");
+  }
+);
+
+router.get("/form/:id", async (req, res) => {
+  const id = req.params.id;
+  const form = await Form.findById(id);
+  const filePath = "uploads/" + form.path;
+  console.log(filePath);
+  fs.readFile(filePath, (err, data) => {
+    res.contentType("application/pdf");
+    res.send(data);
+  });
+});
+
+router.delete("/form/:id", middleware.isLoggedIn, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const form = await Form.findById(id);
+    fs.unlinkSync(`uploads/${form.path}`);
+    console.log("successfully deleted /tmp/hello");
+    await Form.findByIdAndRemove(id);
+    res.redirect("/form");
+  } catch (err) {
+    // handle the error
+    console.log(err);
+    res.redirect("/form");
+  }
+});
+
 
 router.get("/profile", middleware.isLoggedIn, async (req, res) => {
   res.render("profile");
